8fe1c1386d8d1eb8138a4f66f3f46c3c8c8093f2
Michal Conos <michal.conos@gmail.com>
2025-12-23 23:15:01 +0100

    add depthmap processing script for all the scenes

---
+++ src/depth-map-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+CURR_PFX=$(conda info --json |jq .active_prefix_name)
+
+if [ "$CURR_PFX" = '"base"' ]; then
+  cat <<EOH
+  Please use:
+
+    conda activate <your env for video depth>, e.g."
+    conda activate video-depth"
+
+  To list all available environments:
+    conda info --envs
+EOH
+  exit 1
+fi
+
+
+D=$(dirname $0)
+SCENES="$D/../Scenes"
+
+for d in $SCENES/*; do
+  [ -d "$d" ] || continue
+  [ -d "$d/video_depth_map" ] || {
+    echo "Creating depth maps for $d ... "
+    python "$D/video_depth.py" "$d"
+  }
+done
